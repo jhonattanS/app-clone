5f95cd93c10639bc54cd7f2d068969cfff5eb048
Jhonattan S <jhonattan@gmail.com>
2020-01-25 18:59:05 -0300

    atualizar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:uber/telas/HomePage.dart';
 
+final ThemeData temaPadrao = ThemeData(
+  primaryColor: Color(0xff37474f),
+  accentColor: Color(0xff546e7a)
+);
+
 void main() => runApp(MaterialApp(
   home: HomePage(),
+  theme: temaPadrao,
+  title: "Uber",
+  debugShowCheckedModeBanner: false,
 ));
 
 
---
+++ lib/telas/CadastroPage.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+class CadastroPage extends StatefulWidget {
+  @override
+  _CadastroPageState createState() => _CadastroPageState();
+}
+
+class _CadastroPageState extends State<CadastroPage> {
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _controllerEmail = new TextEditingController();
+    TextEditingController _controllerSenha = new TextEditingController();
+    TextEditingController _controllerNome = new TextEditingController();
+    bool _tipoUsuarioPassageiro = false;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cadastro"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextField(
+                  controller: _controllerNome,
+                  keyboardType: TextInputType.text,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "Nome Completo",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6)
+                      )
+                  ),
+                ),
+                TextField(
+                  controller: _controllerEmail,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "E-mail",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6)
+                      )
+                  ),
+                ),
+                TextField(
+                  controller: _controllerSenha,
+                  obscureText: true,
+                  keyboardType: TextInputType.emailAddress,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                      contentPadding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                      hintText: "Senha",
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6)
+                      )
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: <Widget>[
+                      Text("Passageiro"),
+                      Switch(
+                        value: false,
+                        onChanged: (bool valor){
+                          setState(() {
+                            _tipoUsuarioPassageiro = valor;
+                          });
+                        },
+                      ),
+                      Text("Motorista")
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top:16, bottom: 10),
+                  child: RaisedButton(
+                    child: Text(
+                      "Cadatrar",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20
+                      ),
+                    ),
+                    color: Color(0xff1ebbd8),
+                    padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                    onPressed: (){},
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 16),
+                  child: Center(
+                    child: Text(
+                      "Erro",
+                      style: TextStyle(
+                          color: Colors.red,
+                          fontSize: 20
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
